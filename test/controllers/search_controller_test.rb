428a1453e2b66718f673707e54736d946a43b869
Joel Collins <jconline@gmail.com>
2017-07-08 23:50:02 -0400

    clean up controller test

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,50 +1,21 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @search_term = search_terms(:one)
-  # end
-
   test "should get index" do
     get '/'
     assert_response :success
   end
 
-  test "should get search" do
+  test "should do search" do
     VCR.use_cassette("basic_test") do
       get '/search?q=test'
       assert_response :success
     end
   end
 
-  # test "should perform search" do
-  #   assert_difference('SearchTerm.count') do
-  #     post search_terms_url, params: { search_term: {  } }
-  #   end
-
-  #   assert_redirected_to search_term_url(SearchTerm.last)
-  # end
-
-  # test "should show search_term" do
-  #   get search_term_url(@search_term)
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get edit_search_term_url(@search_term)
-  #   assert_response :success
-  # end
-
-  # test "should update search_term" do
-  #   patch search_term_url(@search_term), params: { search_term: {  } }
-  #   assert_redirected_to search_term_url(@search_term)
-  # end
-
-  # test "should destroy search_term" do
-  #   assert_difference('SearchTerm.count', -1) do
-  #     delete search_term_url(@search_term)
-  #   end
-
-  #   assert_redirected_to search_terms_url
-  # end
+  test "should not do search" do
+    get '/search?q=te'
+    assert_response :redirect
+    assert_equal 'Please enter at least 3 characters...', flash[:error]
+  end
 end
